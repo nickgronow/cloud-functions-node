93e0ac95126e2cb3c5e61908087f3946fa7d1fc5
Nick Gronow <nick@wov.io>
2019-09-09 16:26:31 -0400

    Support env name conversions

--- faas.js
+++ faas.js
@@ -2,11 +2,15 @@ const fs = require('fs')
 
 module.exports = {
   secret (name) {
-    const envVar = process.env[name]
+    const envVar = process.env[this.nameToEnvName(name)]
     if (envVar) {
       return envVar
     } else {
       return fs.readFileSync('/var/openfaas/secrets/' + name, 'utf8')
     }
+  },
+
+  nameToEnvName (name) {
+    return name.toUpperCase().replace(/-/g, '_')
   }
 }
