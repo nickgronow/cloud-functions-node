3572f98309e2e2a3101460f9f541e88ff5fb2792
Nick Gronow <nick@wov.io>
2019-09-09 15:16:57 -0400

    Add graphql support

---
+++ graph.js
@@ -0,0 +1,21 @@
+const { query } = require('graphqurl')
+
+export default {
+  headers: {
+    'x-hasura-admin-secret': 'secret',
+    'content-type': 'application/json'
+  },
+
+  query,
+
+  getResults: (name, graphQuery, data) => {
+    return query({
+      query: graphQuery,
+      endpoint: 'http://localhost:8081/v1/graphql',
+      headers: this.headers,
+      variables: data
+    })
+      .then(result => result.data[name])
+      .catch(err => err.message)
+  }
+}
--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const faas = require('./faas')
 const auth = require('./auth')
 const http = require('./http')
 const redis = require('./redis')
+const graph = require('./graph')
 
 module.exports = {
   faas,
   auth,
   redis,
-  http
+  http,
+  graph
 }
