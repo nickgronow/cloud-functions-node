051bc2d24eedfc61bc5be6c3e1de7ba50b0d3fc0
Nick Gronow <nick@wov.io>
2019-09-11 19:13:17 -0400

    Switch graph to use new package

--- graph.js
+++ graph.js
@@ -1,22 +1,17 @@
-const { query } = require('graphqurl')
+const { GraphQLClient } = require('graphql-request')
+
+const endpoint = process.env.GRAPHQL_URL
+
+const headers = {
+  'x-hasura-admin-secret': 'secret',
+  'content-type': 'application/json'
+}
 
 module.exports = {
-  endpoint: process.env.GRAPHQL_URL,
-
-  headers: {
-    'x-hasura-admin-secret': 'secret',
-    'content-type': 'application/json'
-  },
-
-  query,
+  client: new GraphQLClient(endpoint, { headers: headers }),
 
   getResults: function (name, graphQuery, data) {
-    return query({
-      query: graphQuery,
-      endpoint: this.endpoint,
-      headers: this.headers,
-      variables: data
-    })
+    return this.client.request(graphQuery, data)
       .then(result => result.data[name])
       .catch(err => err.message)
   }
