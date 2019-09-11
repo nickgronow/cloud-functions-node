57b0a4406cd0b95ae138a2036c6de0167ffd15f6
Nick Gronow <nick@wov.io>
2019-09-11 19:16:00 -0400

    Remove unused packages in index

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 'use strict'
 
 const faas = require('./faas')
-const auth = require('./auth')
 const http = require('./http')
-const redis = require('./redis')
 const graph = require('./graph')
 
 module.exports = {
   faas,
-  auth,
-  redis,
   http,
   graph
 }
