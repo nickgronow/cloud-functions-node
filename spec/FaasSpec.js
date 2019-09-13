33ca166b444e3e9087fca5f7cb4bc49c82f34525
Nick Gronow <nick@wov.io>
2019-09-13 14:40:43 -0400

    Support a default value for secrets

--- spec/FaasSpec.js
+++ spec/FaasSpec.js
@@ -1,5 +1,5 @@
 describe("Faas", function () {
-  const Faas = require('../faas.js')
+  const Faas = require('../lib/faas.js')
 
   beforeEach(function () {
     this.name = 'a-secret-variable'
@@ -14,18 +14,29 @@ describe("Faas", function () {
   describe('secret', function () {
     describe('when the environment variable exists', function () {
       beforeEach(function () {
-        process.env.A_SECRET_VARIABLE = 'secret'
+        process.env.A_SECRET_VARIABLE = 'secret in a env var'
+      })
+      afterEach(function () {
+        delete process.env.A_SECRET_VARIABLE
       })
 
       it('should return the value of this variable', function () {
-        expect(Faas.secret(this.name)).toBe('secret')
+        expect(Faas.secret(this.name)).toBe('secret in a env var')
       })
     })
 
     describe('when the environment variable does not exist', function () {
       describe('when a file does not exist', function () {
-        it('should raise an error', function () {
-          expect(function () { Faas.secret(this.name) }).toThrow()
+        describe('when a default value is set', function () {
+          it('should return the default value', function () {
+            expect(Faas.secret(this.name, 'default')).toBe('default')
+          })
+        })
+
+        describe('when a default value is not set', function () {
+          it('should raise an error', function () {
+            expect(function () { Faas.secret(this.name) }).toThrow()
+          })
         })
       })
 
@@ -33,7 +44,7 @@ describe("Faas", function () {
         beforeEach(function () {
           const fs = require('fs')
           process.env.FAAS_SECRET_PATH = './'
-          fs.writeFile(this.name, 'secret', () => {})
+          fs.writeFileSync(this.name, 'secret in a file')
         })
         afterEach(function () {
           const fs = require('fs')
@@ -41,7 +52,7 @@ describe("Faas", function () {
           fs.unlink(this.name, () => {})
         })
         it('should return the contents of that file', function () {
-          expect(Faas.secret(this.name)).toBe('secret')
+          expect(Faas.secret(this.name)).toBe('secret in a file')
         })
       })
     })
